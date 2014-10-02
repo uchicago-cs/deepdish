575c30d0495380a7b3e98ff0e07ac47fe466ede6
Mark Stoehr <stoehr.mark@gmail.com>
2014-10-02 18:33:06 -0500

    construct alignments for different data split sets

--- experiments/conv_deep_maxout_TIMIT/generate_training_alignments.sh
+++ experiments/conv_deep_maxout_TIMIT/generate_training_alignments.sh
@@ -8,6 +8,11 @@ echo -e "h#\th#" >> dict2
 
 HVite -f -a -T 1 -C hvite.config -H tri-nmix${bestnumtrimix}-npass4/MMF -S train.scp -I trainMono.mlf -i trainFATri.mlf dict2 tiedlist 
 
-awk '{ if ( NF > 3 ) { print $3 } }' trainFATri.mlf | sort | uniq > triphoneLabels
+HVite -f -a -T 1 -C hvite.config -H tri-nmix${bestnumtrimix}-npass4/MMF -S dev.scp -I devMono.mlf -i devFATri.mlf dict2 tiedlist 
+
+HVite -f -a -T 1 -C hvite.config -H tri-nmix${bestnumtrimix}-npass4/MMF -S coreTest.scp -I coreTestMono.mlf -i coreTestFATri.mlf dict2 tiedlist 
+
+
+cat trainFATri.mlf devFATri.mlf coreTestFATri.mlf | awk '{ if ( NF > 2 ) { print $3 } }'  | sort | uniq > triphoneLabels
 
 
