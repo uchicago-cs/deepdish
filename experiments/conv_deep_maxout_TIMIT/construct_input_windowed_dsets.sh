87f6a605723b4e40bc63da8caa137dbdb9edcb3f
Mark Stoehr <stoehr.mark@gmail.com>
2014-10-14 20:13:07 -0500

    script to create the training, developing, and testing datasets

---
+++ experiments/conv_deep_maxout_TIMIT/construct_input_windowed_dsets.sh
@@ -0,0 +1,6 @@
+python scripts/generate_randomized_hdf5_svm_dataset.py --dset train --seed 0 --n_context_frames 17
+
+python scripts/generate_randomized_hdf5_svm_dataset.py --dset dev --seed 0 --n_context_frames 17
+
+python scripts/generate_randomized_hdf5_svm_dataset.py --dset coreTest --seed 0 --n_context_frames 17
+
