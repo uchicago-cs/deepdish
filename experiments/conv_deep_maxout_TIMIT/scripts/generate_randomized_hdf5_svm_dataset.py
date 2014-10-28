fc467add6c3d049321fd24a69c45f453445fcd9a
Mark Stoehr <stoehr.mark@gmail.com>
2014-10-28 12:21:55 -0500

    shuffled hdf5 svm dataset

--- experiments/conv_deep_maxout_TIMIT/scripts/generate_randomized_hdf5_svm_dataset.py
+++ experiments/conv_deep_maxout_TIMIT/scripts/generate_randomized_hdf5_svm_dataset.py
@@ -26,7 +26,7 @@ F = h5py.File('work/timit_CDlabel_fbank_{0}.hdf5'.format(dset),'r')
 n_frames = F['utt_start_end_inds'][-1,-1]
 X = frame_contexts.utterance_to_contexts(F['data'][:2],n_context_frames)
 
-feature_shape = X.shape
+feature_shape = X.shape[1:]
 F_out = h5py.File('work/timit_CDlabel_fbank_window_{0}.hdf5'.format(dset),'w')
 out_data = F_out.create_dataset('data',shape=(n_frames,) + feature_shape,dtype=np.float32)
 np.random.seed(0)
@@ -35,12 +35,36 @@ out_indices = F_out.create_dataset('indices',data=data_indices)
 
 out_labels = F_out.create_dataset('labels',shape=(n_frames,),dtype=np.int32)
 
+Xmin = np.inf
+Xmax = -np.inf
+
+batch_size=10000
+n_batches = (F['data'].shape[0] -1)//batch_size + 1
+for batch_id in xrange(n_batches):
+    X = F['data'][batch_id*batch_size:min(F['data'].shape[0],
+                                      (batch_id+1)*batch_size)]
+    Xmin = min(Xmin,X.min())
+    Xmax = max(Xmax,X.max())
+
+
+
 datum_id = 0
 for utt_id, (utt_start_ind,utt_end_ind) in enumerate(F['utt_start_end_inds']):
     print utt_id
     use_frames = F['data'][utt_start_ind:utt_end_ind]
     use_labels = F['labels'][utt_start_ind:utt_end_ind]
+    use_frames -= Xmin
+    use_frames /= (Xmax - Xmin)/255.5
     X = frame_contexts.utterance_to_contexts(use_frames,n_context_frames)
+    if utt_id % 10 == 0: print X.shape
+    # X1= frame_contexts.utterance_to_contexts(use_frames[:,:40],n_context_frames)
+    # X2= frame_contexts.utterance_to_contexts(use_frames[:,41:81],n_context_frames)
+    # X3= frame_contexts.utterance_to_contexts(use_frames[:,82:122],n_context_frames)
+    # X1 = X1.reshape(X1.shape[0],40,17,1)
+    # X2 = X2.reshape(X2.shape[0],40,17,1)
+    # X3 = X3.reshape(X3.shape[0],40,17,1)
+    # X = np.concatenate((X1,X2,X3),axis=3).astype(np.uint8)
+
     for x_id, x in enumerate(X):
         shuffled_id = out_indices[datum_id]
         out_data[shuffled_id] = x
