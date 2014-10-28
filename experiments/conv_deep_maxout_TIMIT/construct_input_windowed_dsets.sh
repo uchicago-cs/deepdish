9a0a70b223d2e3ddf0ddb5acb1a89b8eaed23ce9
Mark Stoehr <stoehr.mark@gmail.com>
2014-10-28 12:21:34 -0500

    added random binary features

--- experiments/conv_deep_maxout_TIMIT/construct_input_windowed_dsets.sh
+++ experiments/conv_deep_maxout_TIMIT/construct_input_windowed_dsets.sh
@@ -4,3 +4,4 @@ python scripts/generate_randomized_hdf5_svm_dataset.py --dset dev --seed 0 --n_c
 
 python scripts/generate_randomized_hdf5_svm_dataset.py --dset coreTest --seed 0 --n_context_frames 17
 
+python scripts/generate_rand_binary_svm_dataset.py --dset train --seed 0 --n_context_frames 17 --save_path work/timitCDlabel_fbank_binary_3channel_train.bin
