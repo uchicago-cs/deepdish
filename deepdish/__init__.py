a73da26942e75be7373d0e27b9315d904a0b467a
Gustav Larsson <gustav.m.larsson@gmail.com>
2015-07-19 22:22:18 -0500

    Forgot to mark start of version 0.1.8

--- deepdish/__init__.py
+++ deepdish/__init__.py
@@ -39,8 +39,8 @@ __all__ = ['deepdish',
            'aslice',
            ]
 
-VERSION = (0, 1, 7)
-ISRELEASED = True
+VERSION = (0, 1, 8)
+ISRELEASED = False
 __version__ = '{0}.{1}.{2}'.format(*VERSION)
 if not ISRELEASED:
     __version__ += '.git'
