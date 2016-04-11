44ac3173feb5815866764167d56e463a08ef184c
Gustav Larsson <gustav.m.larsson@gmail.com>
2016-04-10 21:20:46 -0500

    Started 0.3.4

--- deepdish/__init__.py
+++ deepdish/__init__.py
@@ -51,8 +51,8 @@ __all__ = ['deepdish',
            'aslice',
            ]
 
-VERSION = (0, 3, 3)
-ISRELEASED = True
+VERSION = (0, 3, 4)
+ISRELEASED = False
 __version__ = '{0}.{1}.{2}'.format(*VERSION)
 if not ISRELEASED:
     __version__ += '.git'
