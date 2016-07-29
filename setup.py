b700d6afce0e36f599a62126d9fe0db05273dd21
Gustav Larsson <gustav.m.larsson@gmail.com>
2016-07-28 23:11:07 -0700

    REL: 0.3.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ CLASSIFIERS = [
 
 args = dict(
     name='deepdish',
-    version='0.3.3',
+    version='0.3.4',
     url="https://github.com/uchicago-cs/deepdish",
     description="Deep Learning experiments from University of Chicago.",
     maintainer='Gustav Larsson',
